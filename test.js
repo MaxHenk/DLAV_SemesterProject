750d523457ac0842f9fab13d1e3dc129b4f9a0fd
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-10 13:58:55 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-test test
+#!/bin/bash
+#SBATCH --chdir /scratch/<put-your-username-here>
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 1G
+#SBATCH --account scitas-courses
+#SBATCH --reservation intro2clusters
+sleep 10
+echo "hello from $(hostname)"
+sleep 10

3a3977bbfeb50edec36d3057014d26dde7e413f2
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-11 11:18:29 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@
 #SBATCH --partition=gpu
 #SBATCH --gres gpu:1
 #SBATCH --qos=dlav
-#SBATCH --account=civil-459-2023
+#SBATCH --accounts=civil-459-2023
 #SBATCH --reservation=civil-459
 
 print("hello from bpoffet")

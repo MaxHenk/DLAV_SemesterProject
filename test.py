b7621249e73e4375435996398fda3d7892d76bc6
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-11 09:23:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --chdir /scratch/bpoffet/DLAV_SemesterProject
-#SBATCH --ntasks 1
-#SBATCH --cpus-per-task 1
-#SBATCH --mem 8G
-#SBATCH --partition gpu
+#SBATCH --chdir=/scratch/bpoffet/DLAV_SemesterProject
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --partition=gpu
 #SBATCH --gres gpu:1
+#SBATCH --qos=dlav
 #SBATCH --account=civil-459
 #SBATCH --reservation=civil-459
 

3d5f23f1ae03b4597fb74fcfa633c5889490ad68
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-12 15:35:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --chdir=/scratch/bpoffet/DLAV_SemesterProject
 #SBATCH --nodes 1
-#SBATCH --ntasks 1
-#SBATCH --cpus-per-task 1
-#SBATCH --mem 8G
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 20
+#SBATCH --mem 40G
 #SBATCH --partition gpu
 #SBATCH --gres gpu:1
-#SBATCH --qos
+#SBATCH --qos gpu
 #SBATCH --account civil-459
 #SBATCH --reservation dlav
+
 
 print("hello from bpoffet")
 

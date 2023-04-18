c56dc7d4fab9c7915e0ac46697344d644cf9583a
[bappoff] <baptiste.poffet@unil.ch>
2023-04-18 12:40:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --workdir /scratch/bpoffet
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 8G
+#SBATCH --partition gpu
+#SBATCH --gres gpu:1
+#SBATCH --qos dlav
+#SBATCH --account civil-459
+#SBATCH --reservation civil-459

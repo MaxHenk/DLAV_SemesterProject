474531875a463eda38561ee7762946e50519fd2e
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-21 10:00:30 +0200

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 20
+#SBATCH --begin=now
+#SBATCH --mem 40G
+#SBATCH --partition gpu
+#SBATCH --gres gpu:1
+#SBATCH --account civil-459-2023
+
+source venvs/bin/activate
+
+python3 install.py

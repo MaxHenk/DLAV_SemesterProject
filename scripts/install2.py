91348fe403d18802d9c83cd339e834fd59588708
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-21 10:56:59 +0200

    Update install2.py

--- scripts/install2.py
+++ scripts/install2.py
@@ -2,7 +2,7 @@ import subprocess
 
 commands = [
     ['pip', 'install', 'torch==1.9.1', 'torchvision==0.10.1', 'torchaudio==0.9.1', '-f', 'https://download.pytorch.org/whl/torch_stable.html'],
-    ['pip', 'install', '-c', 'omgarcia', 'gcc-6']
+    ['conda', 'install', '-c', 'omgarcia', 'gcc-6']
 ]
 
 for command in commands:

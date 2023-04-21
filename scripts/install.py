0c6057533f243ac1f76fd001e792d01f18fd054b
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-21 10:39:08 +0200

    Update install.py

--- scripts/install.py
+++ scripts/install.py
@@ -2,13 +2,13 @@
 
 #Following https://mmdetection3d.readthedocs.io/en/latest/getting_started.html#installation
 
-
+import pip
 ##a. Create a conda virtual environment and activate it.
 #conda create -n open-mmlab python=3.8 -y
 #conda activate open-mmlab
 
 ##b. Install PyTorch and torchvision following the [official instructions](https://pytorch.org/).
-install torch==1.9.1 torchvision==0.10.1 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.9.1 torchvision==0.10.1 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
 # Recommended torch>=1.9
 
 ##c. Install gcc>=5 in conda env (optional).

369eb9829f0cb4f8f92d3c3af35db7181cbc7dfc
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-21 10:02:33 +0200

    Update install.py

--- scripts/install.py
+++ scripts/install.py
@@ -8,19 +8,19 @@
 #conda activate open-mmlab
 
 ##b. Install PyTorch and torchvision following the [official instructions](https://pytorch.org/).
-$ pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
 # Recommended torch>=1.9
 
 ##c. Install gcc>=5 in conda env (optional).
-$ conda install -c omgarcia gcc-6 # gcc-6.2
+conda install -c omgarcia gcc-6 # gcc-6.2
 
 ##c. Install mmcv-full.
-$ pip install mmcv-full==1.4.0
+pip install mmcv-full==1.4.0
 #  pip install mmcv-full==1.4.0 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.9.0/index.html
 
 ##d. Install mmdet and mmseg.
-$ pip install mmdet==2.14.0
-$ pip install mmsegmentation==0.14.1
+pip install mmdet==2.14.0
+pip install mmsegmentation==0.14.1
 
 ##e. Install mmdet3d from source code.
 git clone https://github.com/open-mmlab/mmdetection3d.git

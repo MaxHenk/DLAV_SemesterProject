c63ef6dfea38157a20ed52e6f9dd1fc4d0275af0
bappoff <93776964+bappoff@users.noreply.github.com>
2023-04-21 10:54:22 +0200

    Update install2.py

--- scripts/install2.py
+++ scripts/install2.py
@@ -1,9 +1,9 @@
 import subprocess
 
-packages = [
-    'torch==1.9.1 torchvision==0.10.1 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html',
-    '-c omgarcia gcc-6'
+commands = [
+    ['pip', 'install', 'torch==1.9.1', 'torchvision==0.10.1', 'torchaudio==0.9.1', '-f', 'https://download.pytorch.org/whl/torch_stable.html'],
+    ['pip', 'install', '-c', 'omgarcia', 'gcc-6']
 ]
 
-for package in packages:
-    subprocess.run(['pip', 'install', package])
+for command in commands:
+    subprocess.run(command)

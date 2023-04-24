c0fbe91b820ce8cfd0b1e864e63c25d1b431c750
MaxHenk <100219851+MaxHenk@users.noreply.github.com>
2023-04-24 18:57:19 +0200

    Update and rename prepare_data_1.sh to prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -19,3 +19,19 @@ pip install imageio
 python label/label_preprocess.py --kitti_root=[SemanticKITTI root] --kitti_preprocess_root=[venv_prepdata_root]
 
 
+#Prerequisite : download file mobilestereonet at https://github.com/NVlabs/VoxFormer/tree/main/preprocess
+#Set current directory to the mobilestereonet folder
+cd mobilestereonet
+#Create virtual environment
+conda env create --file mobilenet.yaml # please modify prefix in .yaml
+conda activate mobilenet
+
+#Script to predict depth map
+./image2depth.sh
+#Script to create pseudo point cloud
+./depth2lidar.sh
+
+conda activate preprocess
+#Script to sweep voxelization
+./lidar2voxel.sh
+

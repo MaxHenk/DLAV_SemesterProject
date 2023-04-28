d273968cc5205d8a2918f7555de24a7ff48d967c
MaxHenk <100219851+MaxHenk@users.noreply.github.com>
2023-04-28 11:04:38 +0200

    Update prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,13 +1,13 @@
 #Prerequisite : dataset has to be downloaded 
 
 #Symlink the dataset root to ./kitti
-ln -s [SemanticKITTI root] ./kitti
+ln -s /work/scitas-share/datasets_civil-459/semantic_kitti/dataset ./kitti
 #Copy the calibration files to the dataset folder in the kitti file
 cp -r data_odometry_calib/sequences kitti/dataset
 
 #create a virtual environment
-python3 -m venv [venv_prepdata_root]
-source [venv_prepdata_root]/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 
 #Install packages
 pip install numpy
@@ -16,8 +16,7 @@ pip install pyyaml
 pip install imageio
 
 #Preprocess the data to generate labels at a lower scale
-python label/label_preprocess.py --kitti_root=[SemanticKITTI root] --kitti_preprocess_root=[venv_prepdata_root]
-
+python label/label_preprocess.py --kitti_root=/work/scitas-share/datasets_civil-459/semantic_kitti/dataset --kitti_preprocess_root=/home/mhenking
 
 #Prerequisite : download file mobilestereonet at https://github.com/NVlabs/VoxFormer/tree/main/preprocess
 #Set current directory to the mobilestereonet folder
